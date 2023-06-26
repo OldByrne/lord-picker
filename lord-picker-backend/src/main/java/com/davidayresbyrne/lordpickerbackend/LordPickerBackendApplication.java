143cbbcf216ac6c6adf077bfae59ef46158fe082
David <davidayresbyrne@gmail.com>
2023-06-26 13:19:22 +0100

    added project proposal, project architecture, architecture diagram, er diagram and base backend springboot project files

---
+++ lord-picker-backend/src/main/java/com/davidayresbyrne/lordpickerbackend/LordPickerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.davidayresbyrne.lordpickerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LordPickerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LordPickerBackendApplication.class, args);
+	}
+
+}
---
+++ lord-picker-backend/src/test/java/com/davidayresbyrne/lordpickerbackend/LordPickerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidayresbyrne.lordpickerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LordPickerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
